e660b99a0c6be62feb613c445c83bf00ec50c0b3
Andreas Schneider <parklez@live.com>
2020-11-30 10:56:42 -0300

    Enhance message parsing algorithm & documentation
    - Message object now has "channel" attribute.
    - The "command" attribute now takes the first word of a message, not necessarely one starting with "!".
    - Regular expression now passes badges if tags are enabled.
    - Add class documentation.

--- parky_bot/models/message.py
+++ parky_bot/models/message.py
@@ -4,36 +4,59 @@ import re
 class Message:
 
     def __init__(self, string):
+        """Message object contains several atributes from Twitch's IRC string,
+        such as:
+            * string (str): IRC string.
+            * message (str): Sender's message.
+            * sender (str): Sender's username.
+            * channel (str): Channel.
+            * badges (dict): dict of available user badges (if tags are enabled).
+                To learn more: https://dev.twitch.tv/docs/irc/tags#privmsg-twitch-tags
+            * targets (list): list of tagged usernames in the message starting with "@".
+            * command (str): First word of the message.
+        Args:
+            string (str): IRC string.
+        """
+
         self.string = string
         self.message = ''
         self.sender = ''
+        self.channel = ''
         self.badges = {}
         self.targets = []
-        self.command = None
-        
-        self.parser()
+        self.command = ''
 
-    def parser(self):
+        self.parse()
+
+    def parse(self):
         if "PRIVMSG" not in self.string.split():
             return
 
-        self.message = re.search(r'PRIVMSG #\w+ :(.*)', self.string).group(1).rstrip()
-        
-        string = self.string.split()
-        badges = string[0].split(';')
-        for item in badges:
-            item = item.split('=')
-            try:
-                self.badges[item[0]] = item[1]
-            except IndexError:
-                pass
-        
-        self.sender = self.badges.get('display-name')
-        
-        if self.message.startswith("!"):
-            self.command = self.message.lower().split()[0]
+        if self.string.startswith('@'):
+            regex = r'@(.*) :(.*)\!.*@.*\.tmi\.twitch\.tv PRIVMSG #(.*) :(.*)'
+            (badges,
+            self.sender,
+            self.channel,
+            self.message) = re.search(regex, self.string).groups()
 
-        words = self.message.split(" ")
+            for item in badges.split(';'):
+                item = item.split('=')
+                try:
+                    self.badges[item[0]] = item[1]
+                except IndexError:
+                    pass
+        else:
+            regex = r':(.*)\!.*@.*\.tmi\.twitch\.tv PRIVMSG #(.*) :(.*)'
+            (self.sender,
+            self.channel,
+            self.message) = re.search(regex, self.string).groups()
+
+        self.sender = self.badges.get('display-name', self.sender)
+
+        words = self.message.split()
+
+        self.command = words[0].lower()
+
         for word in words:
             if word.startswith("@"):
                 self.targets.append(word.strip("@"))
