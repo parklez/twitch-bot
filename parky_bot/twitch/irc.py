0f2bb81035732b5fc3045011afaad5a1a1cf7e16
Andreas Schneider <parklez@live.com>
2021-03-15 17:20:19 -0300

    Improved IRC code
    - Swap .replace() for f strings
    - Added /commands and /membership tags, altho not utilizing them.

--- parky_bot/twitch/irc.py
+++ parky_bot/twitch/irc.py
@@ -27,46 +27,56 @@ class TwitchIRC:
         """Connects to twitch IRC in few steps:
 
         1. Connects to the server
-        2. Sends a token
-        3. Sends the username
-        4. Joins a channel
-        5. Requests extra tags (badges, emotes, etc)
+        2. Requests extra tags (badges, emotes, etc)
+        3. Sends a token
+        4. Sends the username
+        5. Joins a channel
         """
 
         self.irc_sock.connect((self.host, self.port))
+        self.request_tags()
+        #self.request_membership()
+        #self.request_commands()
         self.send_token()
         self.send_nick()
         self.join_channel()
-        self.request_tags()
 
     def disconnect(self):
         self.irc_sock.close()
 
-    def send(self, data: str):
+    def send(self, data: str) -> bool:
         """Converts data into bytes with UTF-8 encoding then socket.send()"""
         try:
             self.irc_sock.send(bytes(data, 'UTF-8'))
+            return True
         except Exception as e:
-            logger.critical(e)
+            logger.error(e)
+            return False
 
     def send_pong(self):
-        self.send("PONG :tmi.twitch.tv\r\n")
+        self.send('PONG :tmi.twitch.tv\r\n')
 
     def send_nick(self):
-        self.send("NICK {}\r\n".format(self.username))
+        self.send(f'NICK {self.username}\r\n')
 
     def send_token(self):
-        self.send("PASS {}\r\n".format(self.token))
+        self.send(f'PASS {self.token}\r\n')
 
     def join_channel(self):
-        self.send("JOIN {}\r\n".format(self.channel))
+        self.send(f'JOIN {self.channel}\r\n')
 
     def part_channel(self):
-        self.send("PART {}\r\n".format(self.channel))
+        self.send(f'PART {self.channel}\r\n')
 
     def send_message(self, message):
-        self.send("PRIVMSG {} :{}\r\n".format(self.channel, message))
+        self.send(f'PRIVMSG {self.channel} :{message}\r\n')
 
     def request_tags(self):
-        #https://discuss.dev.twitch.tv/t/unable-to-register-for-irc-capabilities/27023
-        self.send("CAP REQ :twitch.tv/tags\r\n")
+        # https://discuss.dev.twitch.tv/t/unable-to-register-for-irc-capabilities/27023
+        self.send('CAP REQ :twitch.tv/tags\r\n')
+
+    def request_membership(self):
+        self.send('CAP REQ :twitch.tv/membership\r\n')
+
+    def request_commands(self):
+        self.send('CAP REQ :twitch.tv/commands\r\n')
