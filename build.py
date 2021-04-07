4a631b24aa8e5caabce36aa6f2f372628697c083
Andreas Schneider <parklez@live.com>
2021-04-07 16:53:13 -0300

    Update Travis building process
    Now using python 3.9!
    
    Important note for Windows 7 users:
     - one can still build this project with earlier python versions.

--- build.py
+++ build.py
@@ -3,35 +3,39 @@ import shutil
 import subprocess
 
 
-IGNORE_LIST = ['lib', 'certifi', '_ctypes.pyd', 'app.exe', 'python37.dll', 'base_library.zip']
-PROJECT_PATH = os.getcwd()
-ICON_PATH = os.path.join(PROJECT_PATH, 'art', 'dog_pet_animal_japanese_shiba_inu_japan_icon_127300.ico')
-APP_PATH = os.path.join(PROJECT_PATH, 'dist')
-SCRIPT_PATH = os.path.join(PROJECT_PATH, 'parky_bot', 'app.py')
-"""
-# Pyinstaller build - "--noconsole" to hide terminal
-subprocess.run([
-    "pyinstaller",
-    "-y",
-    "-i", ICON_PATH,
-    "--runtime-hook", "add_lib.py",
-    SCRIPT_PATH,
-    "--noconsole"])
+IGNORE_LIST = ['lib', 'certifi', 'app.exe', 'python39.dll', 'base_library.zip']
+CWD = os.getcwd()
+ICON_PATH = os.path.join(CWD, 'art', 'dog_pet_animal_japanese_shiba_inu_japan_icon_127300.ico')
+SCRIPT_PATH = os.path.join(CWD, 'parky_bot', 'app.py')
+BUILD_PATH = os.path.join(CWD, 'dist', 'app')
+
+subprocess.run(['pyinstaller',
+                '--onedir',
+                '--noconsole',
+                '--icon', ICON_PATH,
+                '--hidden-import', 'gtts',
+                '--hidden-import', 'requests',
+                '--hidden-import', 'audioplayer',
+                '--runtime-hook', 'add_lib.py',
+                SCRIPT_PATH])
 
 # Moving files to 'lib'
-os.makedirs(os.path.join(APP_PATH, 'lib'))
+os.makedirs(os.path.join(BUILD_PATH, 'lib'))
 
-for file in os.listdir(APP_PATH):
+for file in os.listdir(BUILD_PATH):
     if file not in IGNORE_LIST:
-        shutil.move(os.path.join(APP_PATH, file), os.path.join(APP_PATH, 'lib', file))
+        shutil.move(os.path.join(BUILD_PATH, file), os.path.join(BUILD_PATH, 'lib', file))
+
+# Copying core plugins
+shutil.copytree(os.path.join(CWD, 'plugins'), os.path.join(BUILD_PATH, 'plugins'))
 
 # Deleting temporary folders
-shutil.rmtree(os.path.join(PROJECT_PATH, 'build'))
-"""
+shutil.rmtree(os.path.join(CWD, 'build'))
+
 # Archiving
-OUTPUT = os.path.join(PROJECT_PATH, 'parkybot')
+OUTPUT = os.path.join(CWD, 'parkybot')
 shutil.make_archive(base_name=OUTPUT, # Output dir + name
                     format='zip',
                     base_dir='./',  # Base structure
-                    root_dir=APP_PATH # Where to archive from
+                    root_dir=BUILD_PATH # Where to archive from
                     )
